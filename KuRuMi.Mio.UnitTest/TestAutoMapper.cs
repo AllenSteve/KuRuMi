01de6c5029b195f1acdbf48d08c420729abc5211
Edna-lzh <847432003@qq.com>
2017-09-25 10:06:15 +0800

    ddd-v1.0

---
+++ KuRuMi.Mio.UnitTest/TestAutoMapper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuRuMi.Mio.UnitTest
+{
+    [TestClass]
+    public class TestAutoMapper
+    {
+        [TestMethod]
+        public void test() {
+            Entity2 e1 = new Entity2();
+            e1.name = "1";
+            e1.pwd = "2";
+            //AutoMapper.Mapper.Initialize(cfg=>cfg.CreateMap<Entity1, Entity2>());
+            //var s = AutoMapper.Mapper.Map<Entity2>(e1);
+            var s = MapToObj<Entity2, Entity1>(e1);
+            Console.WriteLine(s.name);
+        }
+        public static K MapToObj<T, K>(T obj)
+        {
+            AutoMapper.Mapper.Initialize(t => t.CreateMap<T, K>());
+            return AutoMapper.Mapper.Map<K>(obj);
+        }
+    }
+
+    public class Entity1 {
+        public string name { get; set; }
+        public string pwd { get; set; }
+    }
+
+    public class Entity2 {
+        public string name { get; set; }
+        public string pwd { get; set; }
+    }
+
+}
